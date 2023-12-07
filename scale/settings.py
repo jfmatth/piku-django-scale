00c12ed44f2aa12f56fde79866f5c2a35cef8542
John Matthew <john@compunique.com>
2023-12-07 10:19:54 -0800

    allow everyone for now

--- scale/settings.py
+++ scale/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^rbq2lzxvhy)87d#v(t(k_^+=&ud!mzn6!fn^scc+l_%8wmp!e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
