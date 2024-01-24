638e35d6e043e33df6fe4a7e09f55076e149808d
John Matthew <john@compunique.com>
2024-01-24 07:56:50 -0800

    add hello world

--- scale/urls.py
+++ scale/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from hello import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index, name="homepage")
 ]
+
